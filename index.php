caeae3f730333db0eab36b0f6aff9b86c8d6de52
Christopher McCarthy <chris@Christophers-iMac.local>
2019-10-06 10:33:14 -0700

    created empty flash card template

---
+++ index.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+
+    <!-- Index card template to create new card or update existing  -->
+    <?php require_once 'process.php'; ?>
+    <div class="d-flex justify-content-center">
+      <div class="card p-3 mt-5 w-50">
+        <form action="process.php" method="POST">
+          <div class="form-group">
+            <h3 class="mb-4">Flash Card</h3>
+            <input type="text" name="header" class="form-control" placeholder="Enter card header...">
+            <textarea type="text" name="body" class="mt-2 form-control form-control-textarea" size="500" placeholder="Enter card body..."></textarea>
+            <button type="submit" class="btn btn-danger mt-3 mb-0" name="save">Save</button>
+          </div>
+        </form>
+      </div>
+    </div>
+
+
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  </body>
+</html>
